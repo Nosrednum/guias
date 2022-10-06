1bb784728882a51b863af9fff695ea8609cfd489
mavillam <mavillam@uniandes.edu.co>
2022-10-05 21:42:55 -0500

    Update scriptTarea.sql

--- docs/Tareas/scriptTarea.sql
+++ docs/Tareas/scriptTarea.sql
@@ -1,15 +1,17 @@
 USE Estudiante_43;
 --------------------- Modelo de hecho movimiento
 CREATE TABLE Fecha (
-Fecha DATETIME, 
+ID_Fecha INT,
+Fecha DATE, 
 Dia TINYINT, 
 Mes VARCHAR(20), 
 Anio SMALLINT,
 Numero_semana_ISO TINYINT,
-PRIMARY KEY(Fecha));
+PRIMARY KEY(ID_Fecha));
 
 CREATE TABLE Producto (
-ID_Producto SMALLINT, 
+ID_Producto_DWH SMALLINT, 
+ID_Producto_T SMALLINT, 
 Nombre VARCHAR(100),
 Marca VARCHAR(30), 
 Paquete_de_compra VARCHAR(20), 
@@ -20,21 +22,23 @@ cantidad_por_salida INT,
 Precio_minorista_recomendado FLOAT, 
 Paquete_de_venta VARCHAR(20), 
 Precio_unitario FLOAT,
-PRIMARY KEY(ID_Producto));
+PRIMARY KEY(ID_Producto_DWH));
 
 CREATE TABLE Proveedor(
-ID_proveedor INT
+ID_proveedor_DWH INT
+ID_proveedor_T INT
 Nombre VARCHAR(20),
 Categoria VARCHAR(20),
 Contacto_principal VARCHAR(30),
 Referencia VARCHAR(30),
 Dias_pago INT, 
 Codigo_postal INT,
-PRIMARY_KEY(ID_proveedor)
+PRIMARY_KEY(ID_proveedor_DWH)
 );
 
 CREATE TABLE TipoTransaccion(
-ID_Tipo_transaccion TINYINT,
+ID_Tipo_transaccion_DWH TINYINT,
+ID_Tipo_transaccion_T TINYINT,  
 Tipo VARCHAR(20),
-PRIMARY_KEY(ID_Tipo_transaccion)
+PRIMARY_KEY(ID_Tipo_transaccion_DWH)
 );
